5facaa3222b167094365d72e6d6190ccbcaf47c8
cat-bro <cathjb@gmail.com>
2020-01-21 16:03:01 +1100

    Fix travis file paths and add bioblend installable/installed check
    re-add travis requirements file
    
    travis secrets
    
    travis bioblend wip
    
    squash into prev travis commit
    
    more travis changes
    
    .

--- .travis/check_files.py
+++ .travis/check_files.py
@@ -2,57 +2,179 @@ import argparse
 import yaml
 import sys
 
+from bioblend import ConnectionError
 from bioblend.galaxy import GalaxyInstance
 from bioblend.galaxy.tools import ToolClient
+from bioblend.galaxy.toolshed import ToolShedClient
+from bioblend.toolshed import ToolShedInstance
+from bioblend.toolshed.repositories import ToolShedRepositoryClient
+
+default_tool_shed = 'toolshed.g2.bx.psu.edu'
+
+mandatory_keys = ['name', 'tool_panel_section_label', 'owner']
+allowed_keys = ['tool_shed_url', 'revisions', 'ignore_test_errors']
+forbidden_keys = ['tool_panel_section_id']
+
+def main():
+    parser = argparse.ArgumentParser(description="Lint tool input files for installation on Galaxy")
+    parser.add_argument('-f', '--files', help='Tool input files', nargs='+')
+    parser.add_argument('-u', '--staging_url', help='Galaxy staging server URL')
+    parser.add_argument('-k', '--staging_api_key', help='API key for galaxy staging server')
+    parser.add_argument('-g', '--production_url', help='Galaxy production server URL')
+    parser.add_argument('-a', '--production_api_key', help='API key for galaxy production server')
+
+    args = parser.parse_args()
+    files = args.files
+    staging_url = args.staging_url
+    staging_api_key = args.staging_api_key
+    production_url = args.production_url
+    production_api_key = args.production_api_key
+
+    loaded_files = yaml_check(args.files) # load yaml and raise ParserError if yaml is incorrect
+    key_check(loaded_files)
+    tool_list = join_lists([x['yaml']['tools'] for x in loaded_files])
+    installable_errors = check_installable(tool_list)
+    installed_errors_staging = check_tools_against_panel(staging_url, staging_api_key, tool_list)
+    installed_errors_production = check_tools_against_panel(production_url, production_api_key, tool_list)
+
+    all_warnings = installed_errors_staging  # If a tool is installed on staging but not production, do not raise an exception
+    all_errors = installable_errors + installed_errors_production
+    if all_errors:
+        sys.stderr.write('\n')
+        for error in all_errors:
+            sys.stderr.write('Error %s\n' % error)
+        raise Exception('Errors found')
+    else:
+        sys.stderr.write('All tools are installable and not already installed on %s\n' % production_url)
 
 
-parser = argparse.ArgumentParser(description="Lint tool input files for installation on Galaxy")
-parser.add_argument('-f', '--files', help='Tool input files', nargs='+')
-
-args = parser.parse_args()
-files = args.files
-
-mandatory_keys = [
-    'name', 'tool_panel_section_label', 'tool_shed_url', 'owner'
-]
-
-allowed_keys = ['revisions', 'ignore_test_errors']
-
-forbidden_keys = ['tool_panel_section_id']  # this is unnecessary if we have allowed_keys check
+def join_lists(list_of_lists):
+    return [entry for list in list_of_lists for entry in list]
 
 
-loaded_files = []
-for file in files:
-    with open(file) as file_in:
-        # As a first pass, check that yaml loads
-        loaded_yml = yaml.safe_load(file_in.read()) # might throw exception here
-        loaded_files.append({
-            'yaml': loaded_yml,
-            'filename': file,
-        })
+def flatten_tool_list(tool_list):
+    flattened_tool_list = []
+    for tool in tool_list:
+        if 'revisions' in  tool.keys():
+            for revision in tool['revisions']:
+                copy_of_tool = tool.copy()
+                copy_of_tool['revisions'] = [revision]
+                flattened_tool_list.append(copy_of_tool)
+        else:
+            copy_of_tool = tool.copy()
+            flattened_tool_list.append(copy_of_tool)
+    return flattened_tool_list
 
-for loaded_file in loaded_files:
-    sys.stderr.write('Checking %s ... ' % loaded_file['filename'])
-    if not 'tools' in loaded_file['yaml'].keys():
-        system.out.write('ERROR\n')
-        raise Exception('Expecting .yml file with \'tools\'. Check requests/template/template.yml for an example.')
-    tools = loaded_file['yaml']['tools']
-    if not isinstance(tools, list):
-        tools = [tools]
-    for key in mandatory_keys:
-        for tool in tools:
-            if not key in tool.keys():
-                system.out.write('ERROR\n')
-                raise Exception('All tool list entries must have \'%s\' specified. Check requests/template/template.yml for an example.' % key)
-        if key == 'tool_panel_section_label':
-            pass # TODO: Check that section header is valid
-            #
 
-    sys.stderr.write('OK\n')
+def yaml_check(files):
+    loaded_files = []
+    for file in files:
+        with open(file) as file_in:
+            # As a first pass, check that yaml loads
+            try:
+                loaded_yml = yaml.safe_load(file_in.read()) # might throw exception here
+            except yaml.parser.ParserError as e:
+                raise e
+            loaded_files.append({
+                'yaml': loaded_yml,
+                'filename': file,
+            })
+    return loaded_files
 
-# Use bioblend to check whether the package is already installed.  Need to do this without
-# making the API keys public
+def key_check(loaded_files):  # TODO label check in this method
+    flattened_tools = []
+    for loaded_file in loaded_files:
+        sys.stderr.write('Checking %s \t ' % loaded_file['filename'])
+        if not 'tools' in loaded_file['yaml'].keys():
+            sys.stderr.write('ERROR\n')
+            raise Exception('Expecting .yml file with \'tools\'. Check requests/template/template.yml for an example.')
+        tools = loaded_file['yaml']['tools']
+        if not isinstance(tools, list):
+            tools = [tools]
+        for key in mandatory_keys:
+            for tool in tools:
+                if not key in tool.keys():
+                    sys.stderr.write('ERROR\n')
+                    raise Exception('All tool list entries must have \'%s\' specified. Check requests/template/template.yml for an example.' % key)
+            if key == 'tool_panel_section_label':
+                pass # TODO: Check that section label is valid
+                #
+        sys.stderr.write('OK\n')
 
-# galaxy_instance = GalaxyInstance(url=galaxy_url, key=galaxy_api_key)
-# tool_client = ToolClient(galaxy_instance)
-# panel = tool_client.get_tool_panel()
+
+def check_installable(tools):
+    # Go through all tool_shed_url values in request files and run get_ordered_installable_revisions
+    # to ascertain whether the specified revision is installable
+    errors = []
+    tools_by_shed = {}
+    for tool in tools:
+        if not 'tool_shed_url' in tool.keys():
+            tool.update({'tool_shed_url': default_tool_shed_url})
+        if tool['tool_shed_url'] in tools_by_shed.keys():
+            tools_by_shed[tool['tool_shed_url']].append(tool)
+        else:
+            tools_by_shed[tool['tool_shed_url']] = [tool]
+
+    for shed in tools_by_shed.keys():
+        url = 'https://%s' % shed
+        toolshed = ToolShedInstance(url=url)
+        repo_client = ToolShedRepositoryClient(toolshed)
+
+        for counter, tool in enumerate(tools_by_shed[shed]):
+            try:
+                installable_revisions = repo_client.get_ordered_installable_revisions(tool['name'], tool['owner'])
+                if counter == 0:
+                    sys.stderr.write('Connected to toolshed %s\n' % url)
+                installable_revisions = [str(r) for r in installable_revisions][::-1]  # un-unicode and list most recent first
+                # TODO make absolutely sure that the ordering is now correct
+                if not installable_revisions:
+                    errors.append('Tool with name: %s, owner: %s and tool_shed_url: %s has no installable revisions' % (tool['name'], tool['owner'], shed))
+                    continue
+                shed_status = 'online'
+            except ConnectionError:
+                if counter == 0:
+                    print('Could not connect to toolshed %s\n' % url)
+                shed_status = 'offline'
+                # Raise an exception?  Ask Simon.
+
+            if 'revisions' in tool.keys():  # Check that requested revisions are installable
+                for revision in revisions:
+                    if shed_status == 'online':
+                        if not revision in installable_revisions:
+                            errors.append('% revision %s is not installable' % (tool['name'], revision))
+                    tool.update({'revision_request_type': 'specific', 'shed_status': shed_status})
+            else:
+                if shed_status == 'online':
+                    tool.update({'revisions': [installable_revisions[0]]})
+                tool.update({'revision_request_type': 'latest', 'shed_status': shed_status})
+    return errors
+
+
+def check_tools_against_panel(galaxy_url, galaxy_api_key, tools):
+    galaxy_instance = GalaxyInstance(url=galaxy_url, key=galaxy_api_key)
+    tool_client = ToolClient(galaxy_instance)
+    panel = tool_client.get_tool_panel()
+    requested_tools = flatten_tool_list(tools)
+
+    errors = []
+    # the tool panel returned is a list of sections.
+    # each section is a dict, dict['elems'] is a list of installed tools
+    for section in [p for p in panel if 'elems' in p.keys()]:
+        for elem in section['elems']:
+            if 'tool_shed_repository' in elem.keys():
+                repo = elem['tool_shed_repository']
+                # tool is installed if 'name', 'owner' and 'revision' all match
+                matching_tools = [tool for tool in requested_tools if tool['shed_status'] == 'online' and
+                    (tool['name'], tool['owner'], tool['revisions'][0], tool['tool_shed_url']) ==
+                    (repo['name'], repo['owner'], str(repo['changeset_revision']), repo['tool_shed'])
+                ]
+                if matching_tools:
+                    tool = matching_tools[0]
+                    errors.append(
+                        'Tool with name: %s, owner: %s, revision: %s, tool_shed_url: %s is already installed on %s' %
+                        (tool['name'], tool['owner'], tool['revisions'][0], tool['tool_shed_url'], galaxy_url)
+                    )
+    return errors
+
+
+if __name__ == "__main__": main()
--- .travis/travis.sh
+++ .travis/travis.sh
@@ -1,3 +1,5 @@
+STAGING_URL='https://galaxy-cat.genome.edu.au'  # TODO: when using this in production swap this to 'galaxy-aust-staging' (commented out below)
+PRODUCTION_URL='https://cat-dev.genome.edu.au'  # TODO: when using this in production swap this to 'usegalaxy.org.au' (commented out below)
 STAGING_DIR='galaxy-cat'  # TODO: when using this in production swap this to 'galaxy-aust-staging' (commented out below)
 PRODUCTION_DIR='cat-dev'  # TODO: when using this in production swap this to 'usegalaxy.org.au' (commented out below)
 
@@ -8,31 +10,33 @@ if [ ! $TRAVIS_PULL_REQUEST ] && [ ! "$@" = "local" ]; then
   exit 0;
 fi
 
+if [ "$@" = "local" ]; then
+  TRAVIS_BRANCH=master
+  export $(cat .env)
+fi
+
 # check the range of the commit input_file_paths
 echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST_BRANCH: $TRAVIS_PULL_REQUEST_BRANCH"
-CHANGED_FILES=$(git diff --diff-filter=A --name-only $TRAVIS_BRANCH)
-echo ________________
-echo "git diff --name-only --diff-filter=A $TRAVIS_BRANCH"
-echo $CHANGED_FILES
-echo ________________
-REQUEST_FILES=$(echo $CHANGED_FILES | grep "^requests\/[^\/]*$")
-JENKINS_CONTROLLED_FILES=$(echo $CHANGED_FILES | grep "^(?:\$STAGING_DIR\/|PRODUCTION_DIR\/).*/")
+
+REQUEST_FILES=$(git diff --diff-filter=A --name-only $TRAVIS_BRANCH | cat | grep "^requests\/[^\/]*$")
+JENKINS_CONTROLLED_FILES=$(git diff --name-only $TRAVIS_BRANCH | cat  | grep "^(?:\$STAGING_DIR\/|PRODUCTION_DIR\/).*/")
 
 if [ $JENKINS_CONTROLLED_FILES ]; then
   echo 'Files within $PRODUCTION_DIR or $STAGING_DIR are written by Jenkins and cannot be altered';
   exit 1;
 fi
 
-if [ ! $REQUEST_FILES ]; then
+echo $REQUEST_FILES
+if [ ! "$REQUEST_FILES" ]; then
   echo 'No changed files in requests directory: there are no tests for this scenario';
   exit 0;
 fi
 
 FILE_ARGS=$REQUEST_FILES
-if [ ! -f $REQUESTS_FILES ]; then
-  FILE_ARGS=$(tr "\n" " " < $REQUESTS_FILES)
+if [ ! -f $REQUEST_FILES ]; then
+  FILE_ARGS=$(tr "\n" " " < $REQUEST_FILES)
 fi
 
-# pass the requests file paths to a python script that checks the yml
-python .travis/travis_check_files.py -f $FILE_ARGS
+# pass the requests file paths to a python script that checks the input files
+python .travis/check_files.py -f $FILE_ARGS --staging_url $STAGING_URL --staging_api_key $STAGING_API_KEY --production_url $PRODUCTION_URL --production_api_key $PRODUCTION_API_KEY
